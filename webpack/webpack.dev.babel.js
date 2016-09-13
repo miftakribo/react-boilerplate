44b8720e2b6865a9ef65f4732462577a6229efd1
miftah <dmiftaranger@gmail.com>
2016-09-13 15:24:16 +0700

    fix webpack

--- webpack/webpack.dev.babel.js
+++ webpack/webpack.dev.babel.js
@@ -1,16 +1,15 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: './app/index',
-  output: {
-    filename: 'bundle.js'
-  },
-  devtool: 'source-map',
+  entry: './app/index.js',
+  output: { path: __dirname + '/../app', filename: 'bundle.js' },
   module: {
     loaders: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
         }
